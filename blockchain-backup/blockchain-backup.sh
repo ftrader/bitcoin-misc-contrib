660ac0738225c98630676270497bf7c3590c3af5
ftrader <freetrader@tuta.io>
2017-05-03 01:24:54 +0200

    Remove trailing whitespaces

--- blockchain-backup/blockchain-backup.sh
+++ blockchain-backup/blockchain-backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# This script creates a "snapshot" backup of files in a Bitcoin datadir which 
+# This script creates a "snapshot" backup of files in a Bitcoin datadir which
 # are likely to change when you run again.
 #
-# As most of the block data does not change, this allows the creation 
-# of a relatively small backup which can be used to restore to a certain point 
+# As most of the block data does not change, this allows the creation
+# of a relatively small backup which can be used to restore to a certain point
 # (by simply unpacking this backup and deleting any newer files).
 
-# If the script does not encounter any errors, a timestamped backup archive is 
+# If the script does not encounter any errors, a timestamped backup archive is
 # deposited in the current working directory.
 
 # EXIT CODE:
@@ -42,7 +42,7 @@ cd "$DATADIR"/..
 
 backup_date=$(date "+%Y%m%d_%H%M_%z")
 
-# get list of files except debug logs, conf files and contents of blocks/ 
+# get list of files except debug logs, conf files and contents of blocks/
 datadir_list_reduced=$(find .bitcoin/ | \
                        grep -v .bitcoin/blocks | \
                        egrep -v '(debug\.log|bitcoin\.conf|bitcoind\.pid|LOCK)' | \
@@ -62,7 +62,7 @@ echo $datadir_list_reduced | xargs -n 1 echo > $manifest
 
 # the archived list, since it was filtered out above
 echo $last_blockfiles | xargs -n 1 echo >> $manifest
-# Note: manually adding contents of .bitcoin/blocks/index/ folder to 
+# Note: manually adding contents of .bitcoin/blocks/index/ folder to
 echo $index_reduced | xargs -n 1 echo >> $manifest
 
 backup_file=datadir_snapshot_${backup_date}.tar
@@ -72,7 +72,7 @@ result=$?
 # remove the listing file - all that information can be gleaned from the tarball
 rm -f $manifest
 
-popd 
+popd
 
 if [ $result -eq 0 ]; then
     echo "$(date): created backup file $backup_file"
